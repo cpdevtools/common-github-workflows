acf5bb73c8af471cc7275f630586985dd9cfbc25
Dan Erdelt <erd@conceptual.ca>
2023-03-31 17:39:16 +0000

    cleanup

--- .scripts/lib/create-version-branch.ts
+++ .scripts/lib/create-version-branch.ts
@@ -1,9 +1,15 @@
+import { Octokit } from '@octokit/rest';
+import { SemVer } from 'semver';
 import simpleGit from 'simple-git';
 import { applyVersion } from './apply-version';
 
 
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+});
+
 export async function createVersionBranch(version: string = 'main') {
-    console.log('createVersionBranch', version);
+
 
     if (version !== 'main') {
         const git = simpleGit();
@@ -31,6 +37,19 @@ export async function createVersionBranch(version: string = 'main') {
             await git.add('.');
             await git.commit(`Apply version ${version}`);
             await git.push('origin', versionBranchName);
+
+            const ver = new SemVer(version);
+
+            await octokit.repos.createRelease({
+                owner: 'cpdevtools',
+                repo: 'common-github-workflows',
+                tag_name: `v${version}`,
+                target_commitish: versionBranchName,
+                name: `Release v${version}`,
+                body: `Release v${version}`,
+                draft: false,
+                prerelease: ver.prerelease.length > 0
+            });
         } finally {
             await git.checkout(currentBranchName);
         }
